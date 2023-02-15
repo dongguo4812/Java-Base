5e8bba09db5d251ed5001dc6c323adf3ccc85e9c
dongguo <291320608@qq.com>
2023-02-15 21:38:17 +0800

    Java11 start

---
+++ Java10/src/main/java/com/dongguo/java10/JavaTest.java
@@ -0,0 +1,16 @@
+package com.dongguo.java10;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author dongguo
+ * @date 2023/2/15
+ * @description:
+ */
+public class JavaTest {
+    @Test
+    public void test(){
+        var num = 10;
+//        var s= null;
+    }
+}
